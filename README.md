d9f42abb5107f339d84303428158581d0f8a4fd2
Lucas Ferreira <panchorf@gmail.com>
2016-05-25 18:46:17 -0300

    Starting the module project

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # react-native-progress-indicator
-WIP
+React Native cross-plataform module to create Progress Indicators in your apps.
+
+This module universalized the use of RN modules ActivityIndicatorIOS, ProgressViewIOS and ProgressBarAndroid into a single reusefull View for using both in Android and iOS platforms.
+
+### Installation
+
+```bash
+npm install react-native-progress-indicator --save
+```
+
+## Example
+
+```javascript
+import ProgressIndicator from 'react-native-progress-indicator';
+
+// inside your Components
+<ProgressIndicator size="large" />
+
+// or in progressbar mode
+<ProgressIndicator progress={0.7} />
+```
+
+## License
+MIT
---
+++ index.js
@@ -0,0 +1,51 @@
+/**
+ * @providesModule ProgressIndicator
+ */
+
+'use strict';
+
+import React from 'react';
+import {
+  ActivityIndicatorIOS,
+  ProgressBarAndroid,
+  ProgressViewIOS,
+  Platform
+} from 'react-native';
+
+const _cleanInvalidProps = (props, validProps) => {
+  let newProps = {};
+  for(let k in props) {
+    if(k in validProps) newProps[k] = props[k];
+  }
+
+  return newProps;
+};
+
+const ProgressIndicator = (props) => {
+  if (Platform.OS !== 'ios') { /* isAndroid */
+    const passProps = _cleanInvalidProps(Object.assign({}, props), ProgressBarAndroid.propTypes);
+    if("size" in props) {
+      if(props.size === "large" || props.size === "small") {
+        passProps.styleAttr = (props.size === "large") ? "Large" : "Small";
+      }
+    }
+
+    return <ProgressBarAndroid {...passProps} />;
+  } else {
+    if ("progress" in props) {
+      const passProps = _cleanInvalidProps(Object.assign({}, props), ProgressViewIOS.propTypes);
+      return <ProgressViewIOS {...passProps} />;
+    } else {
+      const passProps = _cleanInvalidProps(Object.assign({}, props), ActivityIndicatorIOS.propTypes);
+      if("styleAttr" in props) {
+        if(props.styleAttr === "Large" || props.styleAttr === "Small") {
+          passProps.size = ("" + props.styleAttr).toLowerCase();
+        }
+      }
+
+      return <ActivityIndicatorIOS {...passProps} />;
+    }
+  }
+};
+
+module.exports = ProgressIndicator;
